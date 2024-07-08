608926c0057a88fbbb101ce3a1865cba8fcbb12a
YoussefElmoudene <elmoudene.ysf@gmail.com>
2024-07-08 18:21:50 +0100

    update schedule event by dragging

--- backend/src/main/java/ma/zs/alc/dao/criteria/core/prof/ScheduleEvent.java
+++ backend/src/main/java/ma/zs/alc/dao/criteria/core/prof/ScheduleEvent.java
@@ -1,13 +1,12 @@
 package ma.zs.alc.dao.criteria.core.prof;
 
 import ma.zs.alc.bean.core.prof.ScheduleProf;
-
-import java.time.LocalDateTime;
+import ma.zs.alc.zynerator.util.DateUtil;
 
 public class ScheduleEvent {
     public Long id;
-    public LocalDateTime start;
-    public LocalDateTime end;
+    public String start;
+    public String end;
     public String title;
     public String backgroundColor;
     public String borderColor;
@@ -16,8 +15,8 @@ public class ScheduleEvent {
 
     public ScheduleEvent(ScheduleProf scheduleProf) {
         this.id = scheduleProf.getId();
-        this.start = scheduleProf.getStartTime();
-        this.end = scheduleProf.getEndTime();
+        this.start = DateUtil.dateTimeToStringForSchedule(scheduleProf.getStartTime());
+        this.end = DateUtil.dateTimeToStringForSchedule(scheduleProf.getEndTime());
         this.title = scheduleProf.getSubject();
         this.backgroundColor = scheduleProf.getColor();
         this.borderColor = scheduleProf.getColor();
