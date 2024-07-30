75721b581b467ac721af63304664724d6ed3bca3
unknown <khalilzouizza0@gmail.com>
2024-07-30 16:09:28 +0100

    fixing chat APIs

--- backend/src/main/java/ma/zs/alc/zynerator/config/chat/WebSocketConfiguration.java
+++ backend/src/main/java/ma/zs/alc/zynerator/config/chat/WebSocketConfiguration.java
@@ -15,7 +15,6 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 		registry.addEndpoint("/stomp-endpoint").setAllowedOriginPatterns("*").withSockJS();
 		///////
 		registry.addEndpoint("/stomp-endpoint").setAllowedOriginPatterns("*");
-
 	}
 
 	@Override
