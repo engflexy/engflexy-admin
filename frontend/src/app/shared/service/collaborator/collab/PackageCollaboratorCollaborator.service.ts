7ca3ae87f8e390434d986c536bb8791e64fd89bd
salmaArn <selma776881@gmail.com>
2024-08-06 18:37:52 +0100

    subscription collaborator

--- frontend/src/app/shared/service/collaborator/collab/PackageCollaboratorCollaborator.service.ts
+++ frontend/src/app/shared/service/collaborator/collab/PackageCollaboratorCollaborator.service.ts
@@ -114,6 +114,9 @@ export class PackageCollaboratorCollaboratorService {
         return myDate;
     }
 
+
+
+
     get API() {
         return environment.apiUrlAlcservice + 'collaborator/packageCollaborator/';
     }
